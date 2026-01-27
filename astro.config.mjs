6ca2c1f78ab653e22d6495a5039e78e709f0c344
sandyfletcher <patrick.a.r.fletcher@gmail.com>
2026-01-27 01:46:54 -0500

    stable tailwind sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,21 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
-import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   // TODO: switch site URL
   site: 'https://schiketanz.fromconcentrate.ca',
   output: 'server',
   adapter: netlify(),
   integrations: [
-    react(), 
-    keystatic(), 
-    mdx(), 
-    sitemap()
+    react(),
+    keystatic(),
+    mdx(),
+    sitemap(),
+    tailwind(),
   ],
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
